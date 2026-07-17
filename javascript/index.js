6432989ff88e3905fd35ffa74822b7ba2368990f
golden-cat-knight <cilormiaw@gmail.com>
2026-07-18 04:37:16 +0700

    element fetch javascript

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+// Ketik kode ini di dalam file index.js kamu (tanpa tag <script>)
+
+function init() {
+    let sidebar = document.getElementById("sidebar");
+    let ul = document.getElementById("ul");
+    let li = document.getElementById("li");
+    setTimeout(() => {
+        if (sidebar && ul) {
+            sidebar.addEventListener("click", () => {
+                sidebar.classList.toggle("sidebar-cancel");
+                ul.classList.toggle("ul-show");
+                li.classList.toggle("li-show");
+            });
+        }
+    }, 50);
+}
+
+fetch("/elemen/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+        init()
+    });
